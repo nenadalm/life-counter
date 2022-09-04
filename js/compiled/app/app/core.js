275c317d94a1c641f156f6d20e1fe49463b983ce
nenadalm <nenadalm@gmail.com>
2022-09-04 12:44:19 +0000

    Deploying to gh-pages from @ nenadalm/life-counter@ec25f10d2f89842d160360065dd4d11869df8f80 🚀

--- js/compiled/app/app/core.js
+++ js/compiled/app/app/core.js
@@ -7,13 +7,6 @@ goog.require('reagent.dom');
 goog.require('app.config');
 goog.require('app.views');
 goog.require('app.events');
-app.core.dev_setup = (function app$core$dev_setup(){
-if(app.config.debug_QMARK_){
-return cljs.core.println.cljs$core$IFn$_invoke$arity$variadic(cljs.core.prim_seq.cljs$core$IFn$_invoke$arity$2(["dev mode"], 0));
-} else {
-return null;
-}
-});
 app.core.mount_root = (function app$core$mount_root(){
 re_frame.core.clear_subscription_cache_BANG_();
 
@@ -28,10 +21,24 @@ return null;
 return G__14239__$1.register("worker.js");
 }
 });
+app.core.dev_setup = (function app$core$dev_setup(){
+if(app.config.debug_QMARK_){
+return cljs.core.println.cljs$core$IFn$_invoke$arity$variadic(cljs.core.prim_seq.cljs$core$IFn$_invoke$arity$2(["dev mode"], 0));
+} else {
+return null;
+}
+});
+app.core.prod_setup = (function app$core$prod_setup(){
+if(app.config.debug_QMARK_){
+return null;
+} else {
+return app.core.register_worker();
+}
+});
 app.core.init = (function app$core$init(){
 app.core.dev_setup();
 
-app.core.register_worker();
+app.core.prod_setup();
 
 re_frame.core.dispatch_sync(new cljs.core.PersistentVector(null, 1, 5, cljs.core.PersistentVector.EMPTY_NODE, [cljs.core.cst$kw$app$events_SLASH_init], null));
 
--- worker.js
+++ worker.js
@@ -1,4 +1,4 @@
-const urlsToCache = ["?a80f45c921be7fc0124ca215e4f441b1692b18d2","index.html?v=a80f45c921be7fc0124ca215e4f441b1692b18d2","js/app.js?v=5d432ae665cb870ae9fa41fceda65991c0a25dec","css/styles.css?v=2020f470d71a74cb50b85d8525b9acf6c23e97c7","img/icon.svg?v=caca79da26faa19b77090e4fa0e18e3a86dcc89f"];
+const urlsToCache = ["?0c8aaf92196ca61d2445f5752c05ac7c4e7bb2e9","index.html?v=0c8aaf92196ca61d2445f5752c05ac7c4e7bb2e9","js/app.js?v=5d432ae665cb870ae9fa41fceda65991c0a25dec","css/styles.css?v=180decbc8ccf38863bcc060a02c65e46c4de2ce2","img/icon.svg?v=caca79da26faa19b77090e4fa0e18e3a86dcc89f"];
 
 self.addEventListener('install', event => {
     event.waitUntil(caches.open('resources').then(cache => cache.addAll(urlsToCache)));
