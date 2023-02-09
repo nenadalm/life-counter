e436c729aa9b5fa5e46a8b0d1647d0a8a0430bf0
nenadalm <nenadalm@gmail.com>
2023-02-09 20:13:15 +0000

    Deploying to gh-pages from @ nenadalm/life-counter@aa3fbd48ad2ac54bccb0d7ea1ffa1c8c841c1c3c 🚀

--- worker.js
+++ worker.js
@@ -1,4 +1,4 @@
-const urlsToCache = ["","index.html","manifest.json","img/icon.svg","js/app.js?v=fe0295b050c46cf6bf8904d8961552fb1a0456b9","css/styles.css?v=91825b87134c6d09d2f35fe334d53b855aad02e3"];
+const urlsToCache = ["","index.html","manifest.json","img/icon.svg","js/app.js?v=45644ce17d759fe9d80f36bce6acb328027f092b","css/styles.css?v=91825b87134c6d09d2f35fe334d53b855aad02e3"];
 
 self.addEventListener('install', event => {
     event.waitUntil(caches.open('resources').then(cache => cache.addAll(urlsToCache)));
