09263b7c25c875fd569ad79fada5aa1b54e46e4c
nenadalm <nenadalm@gmail.com>
2024-01-06 13:19:13 +0000

    Deploying to gh-pages from @ nenadalm/life-counter@c4e2923d953f12a85a407aa1760be0b1e6f0d74a 🚀

--- worker.js
+++ worker.js
@@ -1,7 +1,8 @@
-const relatedAppVersion = "36c0badc5829cbe934d05564b8a51ac3cc8f25a8";
+const relatedAppVersion = "c4e2923d953f12a85a407aa1760be0b1e6f0d74a";
 const urlsToCache = ["index.html","manifest.json","img/icon.svg","js/app.7C040D89C8D45CD847E83929BE3F9348.js","css/styles.9e19bd93a962bf6536dd8278190bfd72978658ef.css"];
 
-const cacheKey = `resources.${relatedAppVersion}`;
+const cacheKeyPrefix = 'nenadalm.life-counter.';
+const cacheKey = `${cacheKeyPrefix}.resources.${relatedAppVersion}`;
 
 function ensureHtmlVersionMatches(cache) {
     return cache.match(new Request('index.html'))
@@ -23,6 +24,7 @@ self.addEventListener('install', event => {
 self.addEventListener('activate', event => {
     event.waitUntil(
         caches.keys()
+            .then(keys => keys.filter(key => key.startsWith(cacheKeyPrefix)))
             .then(keys => keys.filter(key => key !== cacheKey))
             .then(oldKeys => Promise.all(oldKeys.map(key => caches.delete(key))))
     );
