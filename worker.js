337f88965c834116b79b657075559c5bb4aedd07
nenadalm <nenadalm@gmail.com>
2025-07-09 15:52:28 +0000

    Deploying to gh-pages from @ nenadalm/life-counter@f542bb1580a251e3f7a3bbca4b76bdc4d040954f 🚀

--- worker.js
+++ worker.js
@@ -1,5 +1,5 @@
-const relatedAppVersion = "bdcf2b541d28ffa1a7e56c844c5d43ea18a61755";
-const urlsToCache = ["index.html","manifest.json","img/icon.svg","js/app.99310274747291BF7D755E61EAF36346.js","css/styles.3c11952f8de8fc64e3c6fbc842020e806094b187.css"];
+const relatedAppVersion = "f542bb1580a251e3f7a3bbca4b76bdc4d040954f";
+const urlsToCache = ["index.html","manifest.json","img/icon.svg","js/app.BFB83F676120C5850BE3068A31249B91.js","css/styles.3c11952f8de8fc64e3c6fbc842020e806094b187.css"];
 
 const cacheKeyPrefix = 'nenadalm.life-counter.';
 const cacheKey = `${cacheKeyPrefix}resources.${relatedAppVersion}`;
